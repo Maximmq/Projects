b0c262926c0a622063b29955b20d4c1680ee0e3f
Maximmq <totuolmao@gmail.com>
2024-04-16 22:54:09 +0300

    'pz-14'

--- Borzisty/PZ_14/PZ_14_3.py
+++ Borzisty/PZ_14/PZ_14_3.py
@@ -1,10 +1,15 @@
-
 import re
-# input_string = input("Enter Email address : ")
-# regex_pattern = r'\d{3}'
-# result = bool(re.match(regex_pattern, input_string))
-# print("Valid Email address" if result else "Invalid Email address")
+
+cf1, cf2 = 0, 0
 with open('C:\\Users\\Lenovo\\Python projects\\Projects\\Borzisty\\PZ_14\\hotline.txt', 'r+', encoding='utf-8') as f:
-    while True:
-        print(re.search(r'\d{11}$', f.readline()))
-        next(f)
+    for line in f:
+        a = re.search(r'\d{11}$', line)
+        if a:
+            with open('C:\\Users\\Lenovo\\Python projects\\Projects\\Borzisty\\PZ_14\\f1.txt', 'a+', encoding='utf-8') as f1:
+                f1.write(line)
+                cf1 += 1
+        else:
+            with open('C:\\Users\\Lenovo\\Python projects\\Projects\\Borzisty\\PZ_14\\f2.txt', 'a+', encoding='utf-8') as f2:
+                f2.write(line)
+                cf2 += 1
+print(f'Корректные: {cf1}{'\n'}Некорректные: {cf2}')
